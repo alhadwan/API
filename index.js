9538b20c53d9ad4f94021046b36f303d34dd617f
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-04-03 08:00:48 -0700

    add index.ejs,index.js and API

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import axios from 'axios';
+import { name } from 'ejs';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
+
 app.get('/', async (req,res)=>{
-// const response = await axios.get("https://secrets-api.appbrewery.com/random");
-// const data = response.data
-// console.log(data);
-res.render("index.ejs");
+  const name = req.query.name;
+  try{
+    const response = await axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${name}?format=json`);
+const info = response.data
+console.log(info.Results);
+res.render("index.ejs", {data: info.Results});
+  }catch(error){
+
+    console.error("API call failed:", error.message);
+    res.status(500).send("Something went wrong.");
+
+    }
+  
 });
 
 app.listen(port, () => {
